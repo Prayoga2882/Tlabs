ce2be513a218055c75715d7ba32fc1df4566c0d8
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-04-20 14:19:58 +0700

    tlabs

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"go-postgres-menu/controllers"
 	"go-postgres-menu/router"
 	"log"
 	"net/http"
@@ -10,13 +9,6 @@ import (
 
 func main() {
 	r := router.Router()
-	// API Routes
-	r.HandleFunc("/api/menu", controllers.CreateMenu).Methods("POST", "OPTIONS")
-	r.HandleFunc("/api/menu", controllers.GetAllMenu).Methods("GET", "OPTIONS")
-	r.HandleFunc("/api/menu/{id}", controllers.GetMenu).Methods("GET", "OPTIONS")
-	r.HandleFunc("/api/menu/{id}", controllers.UpdateMenu).Methods("PUT", "OPTIONS")
-	r.HandleFunc("/api/menu/{id}", controllers.DeleteMenu).Methods("DELETE", "OPTIONS")
-
 	fmt.Println("Starting server on 8080")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
